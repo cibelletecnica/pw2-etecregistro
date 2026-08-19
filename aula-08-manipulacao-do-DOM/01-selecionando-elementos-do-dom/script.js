5a4e3318ca68e0135e315697a16d6e6fd7083aca
CIBELLE NAOMI OYADOMARI FUKAZAWA <cibelle.fukazawa@e239.local>
2026-08-19 14:15:54 -0300

    m

--- aula-08-manipulacao-do-DOM/01-selecionando-elementos-do-dom/script.js
+++ aula-08-manipulacao-do-DOM/01-selecionando-elementos-do-dom/script.js
@@ -9,3 +9,15 @@ const p0 = document.window.document.getElementsByTagName('p')[0];
 // ALTERANDO AS CARACTERÍSTICAS DO ELEMENTO
 p0.style.color = 'yellow';
 p0.innerText = "Mudei o texto C:";
+
+const p1 = document.getElementsByTagName('p')[1];
+p1.style.color = 'black';
+
+// CAPTURANDO O CORPO DO SITE
+const corpoSite = window.document.body;
+
+// MUDANDO SUA COR
+corpoSite.style.background = "#2F2F2F"
+
+// ACESSANDO O CONTEÚDO DE UM ELEMENTO DOM
+document.write(`<br>No 2º parágrafo do site está escrito assim: ${p1.innerText}`);
