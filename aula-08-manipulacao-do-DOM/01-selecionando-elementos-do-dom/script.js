c029f19592d088d20880240d4df596361b1ec108
CIBELLE NAOMI OYADOMARI FUKAZAWA <cibelle.fukazawa@e239.local>
2026-08-19 14:04:09 -0300

    commit

---
+++ aula-08-manipulacao-do-DOM/01-selecionando-elementos-do-dom/script.js
@@ -0,0 +1,11 @@
+// JAVASCRIPT
+
+// SELECIONANDO UM ELEMENTO PELA TAG
+
+// p = parágrafo 
+// 0 = número do elemento que queremos capturar, no caso o primeiro elemento da página
+
+const p0 = document.window.document.getElementsByTagName('p')[0];
+// ALTERANDO AS CARACTERÍSTICAS DO ELEMENTO
+p0.style.color = 'yellow';
+p0.innerText = "Mudei o texto C:";
